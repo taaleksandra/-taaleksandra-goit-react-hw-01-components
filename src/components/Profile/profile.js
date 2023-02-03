46bb6efef20f346a60b1aa5ff2a7df2796e0b999
taaleksandra <olatarnas@gmail.com>
2023-02-03 22:21:47 +0100

    profile

---
+++ src/components/Profile/profile.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import clsx from 'clsx';
+import css from './Profile.module.css';
+// import PropTypes from 'prop-types';
+
+export const Profile = ({ username, tag, location, avatar, stats }) => (
+  <div className={clsx(css.profile)}>
+    <div className={clsx(css.description)}>
+      <img src={avatar} alt="User avatar" className={clsx(css.avatar)} />
+      <p className={clsx(css.name)}>{username}</p>
+      <p className={clsx(css.tag)}>@{tag}</p>
+      <p className={clsx(css.location)}>{location}</p>
+    </div>
+    <ul className={clsx(css.stats)}>
+      <li>
+        <span className={clsx(css.label)}>Followers</span>
+        <span className={clsx(css.quantity)}>{stats.followers}</span>
+      </li>
+      <li>
+        <span className={clsx(css.label)}>Views</span>
+        <span className={clsx(css.quantity)}>{stats.views}</span>
+      </li>
+      <li>
+        <span className={clsx(css.label)}>Likes</span>
+        <span className={clsx(css.quantity)}>{stats.likes}</span>
+      </li>
+    </ul>
+  </div>
+);
